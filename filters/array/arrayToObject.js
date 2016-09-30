6a0ca9e8ba912976dff624860c8d8475c3d06ce2
pjdufour <pjdufour.dev@gmail.com>
2016-09-30 08:11:08 +0200

    arrayToObject filter

---
+++ filters/array/arrayToObject.js
@@ -0,0 +1,15 @@
+geodash.filters["arrayToObject"] = function()
+{
+  return function(x)
+  {
+    var y = {};
+    if(angular.isArray(x))
+    {
+      for(var i = 0; i < x.length; i++)
+      {
+        y[x[i].name] = x[i].value;
+      }
+    }
+    return y;
+  };
+};
